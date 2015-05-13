d5233c2e2eeb9adac0fdc5b47c54ce0da8e36244
Toni Cárdenas <toni@tcardenas.me>
2015-05-13 17:56:22 +0200

    Got a simple window running. That was a bit tough.
    I started from conrod's examples. I'm not a fan of just copypasting something without understanding what's going on, so I copied and tweaked by hand the lines that I though made sense for just getting a window on the screen.
    
    It failed to compile because some methods were missing. I just figured out that was because the examples were outdated, so I went and tried to have them catch up with the libraries' latest versions. I ended up using sloppily designed APIs that didn't exactly do what I wanted, so nothing worked. All this should get a lot easier as the ecosystem matures.
    
    Finally, I tried to compile the original examples, and they worked! So they weren't outdated at all. Then I realized that the missing methods were implemented _externally_, from other modules that I wasn't importing. That "monkey-patching" feature can be tricky if you're a beginner (just like I am). A compiler error message pointed me out in that direction, even offering candidates for importing. rustc's feedback is great.
    
    Cargo also gave me a hard time. It was retrieving old versions of some libraries, I'm not sure why. After a while of removing caches, rebuilding, etc. it started working as expected, for no reason that I could see.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,46 @@
 extern crate sheets;
+extern crate window;
+extern crate glutin_window;
+extern crate opengl_graphics;
+extern crate conrod;
+extern crate event;
 
 fn main() {
-    println!("Hello, world!");
-    println!("{}", sheets::parser::foo());
+    use glutin_window::GlutinWindow;
+    use opengl_graphics::{GlGraphics, OpenGL};
+    use opengl_graphics::glyph_cache::GlyphCache;
+    use window::{WindowSettings, Size};
+    use event::*;
+    use conrod::{Background, Colorable, Theme, Ui, Widget};
+    use std::path::Path;
+
+    let opengl = OpenGL::_3_2;
+    let window = GlutinWindow::new(
+        opengl,
+        WindowSettings::new(
+            "Hello Conrod".to_string(),
+            Size{width: 800, height: 600}
+        )
+    );
+
+    let event_iter = window.events().ups(180).max_fps(60);
+    let mut gl = GlGraphics::new(opengl);
+    let font_path = Path::new("./assets/NotoSans-Regular.ttf");
+    let theme = Theme::default();
+    let glyph_cache = GlyphCache::new(&font_path).unwrap();
+    let ui = &mut Ui::new(glyph_cache, theme);
+
+    for event in event_iter {
+        ui.handle_event(&event);
+        if let Some(args) = event.render_args() {
+            gl.draw(args.viewport(), |_, gl| {
+                // Draw the background.
+                Background::new().rgb(0.2, 0.25, 0.4).draw(ui, gl);
+
+                // Draw our Ui!
+                ui.draw(gl);
+
+            });
+        }
+    }
 }
